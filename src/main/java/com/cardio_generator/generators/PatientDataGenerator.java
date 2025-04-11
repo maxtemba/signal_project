268d6c40514c73cdd473d807aafe998211fab46f
max <t.kirchner@student.maastrichtuniversity.nl>
2025-04-11 10:16:51 +0200

    added doc using google java style for javadoc.

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,21 @@
+/**
+ * Interface for generating patient health data.
+ * Implementation simulates blood levels, blood pressure, blood saturation and ECG.
+ * Uses different output strategies (for example console output).
+ *
+ */
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Health data generation for individual patient.
+ */
 public interface PatientDataGenerator {
+    /**
+     *
+     * @param patientId The ID of the patient for the data to be generated for.
+     * @param outputStrategy User defined output strategy.
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
