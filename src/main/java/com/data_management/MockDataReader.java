70716ecfb48ea61f176ccf3b3aee2dd4bdeebe6c
max <t.kirchner@student.maastrichtuniversity.nl>
2025-05-02 12:38:02 +0200

    Adapted to the changes in DataReader interface

--- src/main/java/com/data_management/MockDataReader.java
+++ src/main/java/com/data_management/MockDataReader.java
@@ -2,9 +2,16 @@ package com.data_management;
 
 import java.io.IOException;
 
+/**
+ * Mock implementation without any functions.
+ */
 public class MockDataReader implements DataReader {
-    @Override
-    public void readData(DataStorage dataStorage) throws IOException {
 
+    @Override
+    public void start() throws IOException {
+    }
+
+    @Override
+    public void setDataStorage(DataStorage dataStorage) {
     }
 }
